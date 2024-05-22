c3e76ddb95482ea0a2ff69f8d029bb6d759e4c45
reduno96 <reduno96@gmail.com>
2024-05-22 18:31:44 +0100

    modication

--- client.c
+++ client.c
@@ -6,17 +6,17 @@
 /*   By: rel-mora <rel-mora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 15:04:57 by rel-mora          #+#    #+#             */
-/*   Updated: 2024/05/19 20:28:26 by rel-mora         ###   ########.fr       */
+/*   Updated: 2024/05/22 18:29:36 by rel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void 	ft_handle_enter(int ac, char **av)
+void	ft_handle_enter(int ac, char **av)
 {
 	int	pid;
-    // int return_kill;
 
+	int return_kill;
 	if (ac != 3)
 	{
 		ft_printf("the number of the argument to entered is not allowed  :(\n");
@@ -28,70 +28,56 @@ void 	ft_handle_enter(int ac, char **av)
 		ft_printf("The Number To Enter is Negative Or 0 :(\n");
 		exit(1);
 	}
-    // return_kill = kill(pid, SIGUSR1);
-	// if (return_kill == -1)
-	// {
-	// 	ft_printf("Invalide PID\n");
-	// 	exit(1);
-	// }
-	// return (pid);
-}
-void ft_send_len(char *av)
-{
-	int len;
-	int count_len;
-	int i;
-	int tmp;
-
-	i = 0;
-	len = ft_strlen(av);
-	tmp = 0;
-	count_len = 0;
-	while(i <= 32)
+	return_kill = kill(pid, SIGUSR1);
+	if (return_kill == -1)
 	{
-		// ft_printf("%d", tmp);
-		tmp = len << i & 1;
-		i++;
+		ft_printf("Invalide PID\n");
+		exit(1);
 	}
-
 }
 
-void ft_send_char(char *av, int pid)
+void	ft_send_char(char *av, int pid)
 {
-	int i = 128;
-	int j = 0;
-	while(av[j])
+	int	i;
+	int	j;
+
+	i = 128;
+	j = 0;
+	while (av[j])
 	{
-		while(i > 0)
+		while (i > 0)
 		{
-			if (av[j]>= i)
-				kill(pid, SIGUSR1);
-			else
+			if (av[j] >> i & 1)
 			{
 				kill(pid, SIGUSR2);
-				av[j] -= i;
+				usleep(200);
 			}
-			i /= 2;
+			else
+			{
+				kill(pid, SIGUSR1);
+				usleep(200);
+			}
 		}
 		j++;
 	}
-
 }
-void ft_send(char *av, int pid)
+void	ft_send(char *av, int pid)
 {
-	int len;
-
-	len = ft_strlen(av);
+	int	i;
 
 	ft_send_char(av, pid);
-	// ft_send_len(av);
-	// while()
+	i = 0;
+	while (i++ < 8)
+	{
+		kill(pid, SIGUSR1);
+		usleep(200);
+	}
 }
 int	main(int ac, char **av)
 {
-	int pid;
+	int	pid;
 
-    ft_handle_enter(ac, av);
+	ft_handle_enter(ac, av);
 	pid = ft_atoi(av[1]);
 	ft_send(av[2], pid);
 	return (0);
